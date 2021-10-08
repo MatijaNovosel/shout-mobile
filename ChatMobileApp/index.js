e43a9bf2425ef463fb7b40bb82a2a7ca584ca9a8
Matija Novosel <mnovosel5@gmail.com>
2021-10-09 00:09:46 +0200

    Added Paper UI

--- ChatMobileApp/index.js
+++ ChatMobileApp/index.js
@@ -1,9 +1,24 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import * as React from 'react';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+};
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
