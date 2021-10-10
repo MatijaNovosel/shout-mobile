212b9033cb2f26382a2e2794b4308c23a340539a
Matija Novosel <mnovosel5@gmail.com>
2021-10-10 15:27:57 +0200

    Chat details wip

--- ChatMobileApp/src/chatDetails.tsx
+++ ChatMobileApp/src/chatDetails.tsx
@@ -1,22 +1,65 @@
-import React from "react";
-import { SafeAreaView, ScrollView, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
+import { FAB } from "react-native-paper";
 import { ChatMessage } from "./components/Chat/ChatMessage";
 import { ChatMessage as ChatMessageModel } from "./models/chat";
+import { loremIpsum } from "./utils/constants";
 import { randInt, range } from "./utils/helpers";
+
+const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "#1F2C34"
+  }
+});
 
 export const ChatDetails = () => {
   const messages: ChatMessageModel[] = range(0, 15, 1).map((n) => ({
-    content: `Message ${n}`,
+    content: loremIpsum.substring(0, randInt(40, loremIpsum.length)),
     sentAt: new Date(),
     userId: randInt(0, 2)
   }));
 
+  let scrollViewRef: ScrollView | null = null;
+
+  const [FABVisible, setFABVisible] = useState<boolean>(true);
+
+  const scrollToEnd = () => {
+    scrollViewRef?.scrollToEnd({ animated: false });
+  }
+
+  useEffect(() => {
+    scrollToEnd();
+  }, [])
+
   return (
     <SafeAreaView>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
+      <FAB
+        small
+        visible={FABVisible}
+        style={styles.fab}
+        icon="arrow-down"
+        onPress={scrollToEnd}
+      />
+      <ScrollView onScroll={(e) => {
+          let paddingToBottom = 10;
+          paddingToBottom += e.nativeEvent.layoutMeasurement.height;
+          if (e.nativeEvent.contentOffset.y >= e.nativeEvent.contentSize.height - paddingToBottom) {
+            // setFABVisible(false);
+          } else {
+            // setFABVisible(true);
+          }
+        }}
+        ref={ref => scrollViewRef = ref}
+        contentInsetAdjustmentBehavior="automatic"
+      >
         <View style={{
           flex: 1,
           flexDirection: "column",
+          padding: 14
         }}>
           {messages.map((msg, i) => <ChatMessage {...msg} key={i} />)}
         </View>
